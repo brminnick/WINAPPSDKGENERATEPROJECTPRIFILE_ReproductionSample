7e642ca29223b85f07811cef813dcb3bc118b2a2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-02-16 09:32:24 -0800

    Add `MauiApp1`

---
+++ MauiApp1/MauiApp1/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Maui;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>().UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		return builder.Build();
+	}
+}
